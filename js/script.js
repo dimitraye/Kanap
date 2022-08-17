92199a31e645b4b16c69201c1a65ffc0aa1021a6
dimitraye <dimitraye94@gmail.com>
2022-08-17 11:45:06 +0200

    amélioration des fonctions dans script

--- js/script.js
+++ js/script.js
@@ -10,39 +10,54 @@ const DOMAIN = '/api/products';
 
 /* DECLARATION DES FONCTIONS */
 
-//fonction qui génère le code HTML pour afficher un produit sur la page
-function dislayProduct(product) {
-  console.log("Produit : ", product);
-  document.getElementById("items").innerHTML += ` <a href="./product.html?id=${product._id}">
+async function dislayProducts() {
+  let jsonListProduct = await getProducts();
+  generateHtmlForProducts(jsonListProduct);
+}
+
+//récupère les products dans l'API
+async function getProducts() {
+  try {
+    let response = await fetch(HOST + DOMAIN);
+    if (response.ok) {
+      let data = await response.json();
+      console.log('products :', data);
+      return data;
+    } else {
+      console.error('Retour du server :', response.status);
+    }
+  } catch (error) {
+    console.log('Erreur dans getProducts() :', error);
+  }
+
+}
+
+//fonction qui génère le code HTML pour afficher les produits sur la page
+function generateHtmlForProducts(jsonListProduct) {
+  for (let jsonProduct of jsonListProduct) {
+    //créé un objet produit à partir d'un produit au format JSON
+    let product = new Product(jsonProduct);
+    console.log("Produit : ", product);
+    document.getElementById("items").innerHTML += ` <a href="./product.html?id=${product._id}">
             <article>
               <img src="${product.imageUrl}" alt="${product.altTxt}">
               <h3 class="productName">${product.name}</h3>
               <p class="productDescription">${product.trunc(50)}</p>
             </article>
           </a> `;
+  }
+
 }
 
 /*---------------------------------------------------------------------------------------- */
 
 
 /* APPEL DES FONCTIONS */
+dislayProducts();
 
-//récupère les products dans l'API
-fetch(HOST + DOMAIN)
-  .then(res => res.json())
-  .then(jsonListProduct => {
-    console.log("jsonListProduct : ", jsonListProduct);
-    for (let jsonProduct of jsonListProduct) {
-      //créé un objet produit à partir d'un produit au format JSON
-      let product = new Product(jsonProduct);
-      dislayProduct(product);
-    }
-  })
-  .catch(function (err) {
-    console.log("Une erreur est survenue.", err); /* indique en détail l'erreur */
-  });
 
 /*---------------------------------------------------------------------------------------- */
+
 
 
 
